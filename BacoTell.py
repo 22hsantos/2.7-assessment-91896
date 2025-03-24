f12f7b5740ab397962a0235d0aa2bf06b0198cc4
22hsantos <22hsantos@wakatipu.school.nz>
2025-03-25 09:36:12 +1300

    summary

---
+++ BacoTell.py
@@ -0,0 +1,12 @@
+
+'''
+
+My program will be a menu and ordering system 
+for the parody fast food chain “Baco Tell”.
+The program allows the user to look through
+the food menu, add food to their cart,
+customise any food item, and also 
+customise delivery options.
+
+'''
+

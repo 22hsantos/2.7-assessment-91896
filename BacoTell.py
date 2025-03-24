e86e15efff8e2a720c064e970f0f1e83f56c7ec6
22hsantos <22hsantos@wakatipu.school.nz>
2025-03-25 09:57:18 +1300

    Update BacoTell.py

--- BacoTell.py
+++ BacoTell.py
@@ -11,14 +11,40 @@ customise delivery options.
 '''
 
 def menu():
-    print('Welcome to Baco Tell')
-    print('How can we help you today?')
-    print('Tacos')
-    print('Burritos')
-    print('Snacks & Sides')
-    print('Desserts')
-    print('Drinks')
-    menu_input = input('>')
+    while True:
+        print('Welcome to Baco Tell')
+        print('How can we help you today?')
+        print('1. Tacos')
+        print('2. Burritos')
+        print('3. Snacks & Sides')
+        print('4. Desserts')
+        print('5. Drinks')
+        menu_input = input('>')
+
+        if menu_input == '1':
+            taco_menu()
+            break
+
+        if menu_input == '2':
+            burrito_menu()
+            break
+
+        if menu_input == '3':
+            snack_side_menu()
+            break
+
+        if menu_input == '4':
+            dessert_menu()
+            break
+
+        if menu_input == '5':
+            drink_menu()
+            break
+
+        else:
+            print('Please enter only numbers 1-5.')
+
+
 
 
 menu()
